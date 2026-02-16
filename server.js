84bc043e133f4e327a034bb5945de9da9046c6ab
rishabh <sainirishabh394@gmail.com>
2026-02-16 12:37:18 +0530

    this is a render backend page

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend is running ");
+});
+
+app.get("/home",(req,res)=>{
+    res.send("hello this is node js server runnig on port 6000")
+})
+
+console.log("running.....")
+app.listen(6000)
